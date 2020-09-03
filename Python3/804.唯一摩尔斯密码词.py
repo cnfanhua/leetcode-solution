b00e4e123a60e9acf5bab79bc6a9a63af661b45e
fanhua <axcjj@hotmail.com>
2020-09-03 11:00:58 +0800

    804.uniqueMorseRepresentations

---
+++ Python3/804.唯一摩尔斯密码词.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=804 lang=python3
+#
+# [804] 唯一摩尔斯密码词
+#
+
+# @lc code=start
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        dic = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--',
+               'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..'}
+        result = set()
+        for word in words:
+            tmp = ''
+            for w in word:
+                tmp += dic[w]
+            result.add(tmp)
+        return len(result)
+# @lc code=end
